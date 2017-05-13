d1ac89cd26aa656febe26cc4f76a5a13b153f7cf
Peter Natale <panatale1@gmail.com>
2017-05-13 17:32:11 -0400

    updates initial setup script, updates .gitignore

--- initial_setup.sh
+++ initial_setup.sh
@@ -1,6 +1,6 @@
-#!/bin/bash
+#1/bin/bash
 
-if [[ $(id -u_ -ne 0 ]]; then
+if [[ $(id -u) -ne 0 ]]; then
 	echo "Please run as root"
 	exit 1
 fi
@@ -9,15 +9,14 @@ if [ $(which python) == "" ]; then
 else
 	echo "Python already installed"
 fi
-pip = $(which pip)
-if [["$?" -ne 0 ]]; then
-	apt-get install --upgrade python-pip
+pip=$(which pip)
+if [["$?" -ne 0]]; then
+	apt-get install -u python-pip
 fi
 pip install virtualenv
 pip install virtualenvwrapper
 
 mkdir -p ~/.virtualenvs
-
 if [[ $(grep WORKON_HOME ~/.bashrc) -ne 0 ]]; then
 	echo "export WORKON_HOME=~/.virtualenvs" >> ~/.bashrc
 fi
