9f58f0c0e876e86757c5c72a4fb1b32af697e976
Peter Natale <panatale1@gmail.com>
2017-05-29 19:43:45 -0400

    updates gunicorn_start, adds batch_import

--- timeclock/gunicorn_start.sh
+++ timeclock/gunicorn_start.sh
@@ -1,25 +1,29 @@
 #!/bin/bash
 
 NAME="timeclock"
-DJANGODIR=/home/repos/timeclock/timeclock
-SOCKFILE=/home/repos/timeclock/run/gunicorn.sock
-NUM_WORKERS=5
+DJANGODIR=/home/peter/repos/timeclock/timeclock
+SOCKFILE=/home/peter/repos/timeclock/run/gunicorn.sock
+NUM_WORKERS=3
 DJANGO_SETTINGS_MODULE=timeclock.settings
 DJANGO_WSGI_MODULE=timeclock.wsgi
 
 # Activate virtualenv
 cd $DJANGODIR
+WRAPPER_PATH=$(which virtualenvwrapper.sh)
+source $WRAPPER_PATH
 workon timeclock
+echo "$DJANGODIR"
 export DJANGO_SETTINGS_MODULE=$DJANGO_SETTINGS_MODULE
 export PYTHONPATH=$DJANGODIR:$PYTHONPATH
 
 RUNDIR=$(dirname $SOCKFILE)
+echo "$RUNDIR"
 test -d $RUNDIR || mkdir -p $RUNDIR
 
 exec gunicorn ${DJANGO_WSGI_MODULE}:application \
 	--name $NAME \
 	--workers $NUM_WORKERS \
-	#--bind=unix:$SOCKFILE \
-	--bind 0.0.0.0:8000
+	--bind=unix:$SOCKFILE \
+	#--bind 127.0.0.1:8000
 	--log-leve=debug \
 	--log-file=-
